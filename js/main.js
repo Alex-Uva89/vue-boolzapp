22ee2b03169a58fa34e6db7575010fb30140be55
Alex-Uva89 <a.uva1988@gmail.com>
2022-03-17 12:22:34 +0100

    restart application vue and not raw JS

--- js/main.js
+++ js/main.js
@@ -2,183 +2,280 @@
 const app = new Vue({
     el:'#js-root',
     data:{
-        counter: 0,
-        newTxt:'',
-        icons: [
+        stringDate: '',
+        activeChat: 0,
+        mainUser: [
             {
-                preFixMicrophone: 'fa-solid',
-                microphone: 'fa-4',
+                name: 'Alessandro Uva',
+                propic: '2',
             }
         ],
-        contacts: [
+        users: [
             {
-                name: 'Henri Kapidani',
-                isVisible: false,
+                name: 'Harry Potter',
+                lastConnect: '2020-01-10T15:30:55',
                 proPic: '8',
                 message: [
                     {
+                        date: '2020-01-10T15:30:55',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'ehi ciao ho bisogno di aiuto!',
-                        txtReply: 'Ciao, certamente!',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Gabry Spanu',
-                isVisible: false, 
+                name: 'Lord Voldemort',
+                lastConnect: '2020-01-10T15:30:55',
                 proPic: '7',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'aiutoooooo non mi riesce nulla',
-                        txtReply: 'Ajò Ale! che succede?',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: '...',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Mauro Costantinescu',
-                isVisible: false, 
+                name: 'Ron',
+                lastConnect: '2020-01-10T15:30:55',
                 proPic: '6',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'Ciao Mauro, sto cercando di far apparire i messaggi con il v-for forse sto sbagliando qualcosa.',
-                        txtReply: '42!',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Paolino ❤',
-                isVisible: false, 
+                name: 'Hermione',
+                lastConnect: '2020-01-10T15:30:55',
                 proPic: '5',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'Amo, sto partendo da casa',
-                        txtReply: 'ok amo.',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Chiara Passano',
-                isVisible: false, 
-                proPic: '2',
+                name: 'Draco Malfoy',
+                lastConnect: '2020-01-10T15:30:55',
+                proPic: '4',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'ciao Chiara, mi sto disperando!',
-                        txtReply: 'Ciao, ale! Ricordati che non devi lavorare troppo.',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Luca',
-                isVisible: false, 
+                name: 'Luna',
+                lastConnect: '2020-01-10T15:30:55',
                 proPic: '3',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'Il termosifone perde di nuovo',
-                        txtReply: 'Chiudi tutto subito altrimenti ci arriva una stangata di gas',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Angelo Lecce',
-                isVisible: false, 
-                proPic: '4',
+                name: 'Neville Packioc',
+                lastConnect: '2020-01-10T15:30:55',
+                proPic: '2',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'Tresor, sabato ci sei?',
-                        txtReply: "Si tesoro, mi sono già messo d'accordo con luca",
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
             {
-                name: 'Angelo Lecce',
-                isVisible: false, 
-                proPic: '4',
+                name: 'Chou',
+                lastConnect: '2020-01-10T15:30:55',
+                proPic: '1',
                 message: [
                     {
                         date: '10/01/2020', 
                         hour: '15:30:55',
-                        lastConnect: '2020/01/10T15:30:55',
-                        hourReply: '15:45:47',
-                        txt: 'Tresor, sabato ci sei?',
-                        txtReply: "Si tesoro, mi sono già messo d'accordo con luca",
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
                     
                     },
                 ]
             },
-            
+            {
+                name: 'Ginny ❤',
+                lastConnect: '2020-01-10T15:30:55',
+                proPic: 'io',
+                message: [
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'ehi ciao ho bisogno di aiuto!',
+                        send: true,
+                    
+                    },
+                    {
+                        date: '10/01/2020', 
+                        hour: '15:30:55',
+                        txt: 'Ciao, certamente!',
+                        send: false,
+                    
+                    },
+                ]
+            },
             
         ]
     },
     methods: {
+        addPropic(){
+           const img = this.mainUser[0].propic;
+           return img;
+        },
         openChat(index){
-           this.counter = index; 
+           this.activeChat = index; 
         },
-        hourFormat(){
-            let time = luxon.DateTime.add(this.lastConnect).toRelativeCalendar();
-            return time
+        hourFormat(date){
+        let formatHourStr = 'dd/MM/yyyy HH:mm:ss'; 
+        return luxon.DateTime.fromISO(date).toFormat(formatHourStr);
         },
-        addTxt(){
-            if(this.newTxt.trim() != ''){
-                const h = new Date();
-                const m = new Date();
-                const s = new Date();
-                let hr = h.getHours();
-                let min = m.getMinutes();
-                let sec = s.getSeconds();
-                let eleContainer = document.querySelector('.widow-chat')
-                let spaceReply = document.createElement('div');
-                spaceReply.classList.add('my-msg');
-                spaceReply.innerHTML = '<div class="text-ms">'+this.newTxt+'</div> <div class="date-ms">'+hr+'.'+min+'.'+sec+'</div>';
-                eleContainer.append(spaceReply);
-                newTxt = ' ';
-                const auto = setTimeout(this.autoReply, 2000);
-            }
-        },
-        autoReply(){
-                const m = new Date();
-                const s = new Date();
-                let min = m.getMinutes();
-                let sec = s.getSeconds();
-                let eleContainer = document.querySelector('.widow-chat')
-                let spaceReply = document.createElement('div');
-                spaceReply.classList.add('reply');
-                spaceReply.innerHTML = '<div class="text-ms">eh certo! cit. Pamela Prati</div> <div class="date-ms">'+min+'.'+sec+'</div>';
-                eleContainer.append(spaceReply);
-        }
+        // addTxt(){
+           
+        // },
+        // autoReply(){
+                
+        // }
     
     }
  
